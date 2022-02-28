643f40212b796b1b16506198728d75fdc7725366
monalikardile <100512644+monalikardile@users.noreply.github.com>
2022-02-28 17:38:06 +0530

    update signup pom class

--- SignupPOM.java
+++ SignupPOM.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 public class SignPOM {
-	
+	//pom class is here
 	//Declaration
 	
 	@FindBy(xpath="//input[@name='firstname']") private WebElement first;
