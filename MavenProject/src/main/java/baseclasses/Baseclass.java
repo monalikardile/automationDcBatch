6ee42a51e77df6485605a77d9eb4bdac77a527b6
kardileshubham <kardileshubham2596@gmail.com>
2022-03-15 16:56:14 +0530

    new project is assigned

--- MavenProject/src/main/java/baseclasses/Baseclass.java
+++ MavenProject/src/main/java/baseclasses/Baseclass.java
@@ -1,5 +1,26 @@
 package baseclasses;
 
-public class Baseclass {
+import java.io.File;
+import java.io.IOException;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import testProject.Configurations;
+
+public class Baseclass {
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver",Configurations.driverpath);
+		WebDriver driver = new ChromeDriver();
+		driver.get(Configurations.appURL);
+		driver.manage().window().maximize();
+		
+	     //sourcefile
+		File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		
+		File f = new File ("D:\\software testing\\automation\\screenshot\\image.jpg");
+		FileHandler.copy(source, f);
 }
--- MavenProject/src/main/java/configurations/PathConfig.java
+++ MavenProject/src/main/java/configurations/PathConfig.java
@@ -1,5 +1,29 @@
 package configurations;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import testProject.Configurations;
+
 public class PathConfig {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",Configurations.driverpath);
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://demoqa.com/alerts");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		
+	WebElement btn = driver.findElement(By.xpath("//button[@id='confirmButton']"));
+	
+	btn.click();
+
+	Alert alt = driver.switchTo().alert();
+	alt.getText();  //get text on popup
+	alt.accept();  //Click on OK
+;	}
 
 }
